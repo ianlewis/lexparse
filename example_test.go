f98035e37e297ece1474005eaa0b698d31ddd606
Ian Lewis <ianmlewis@gmail.com>
2024-01-19 06:07:05 +0000

    test: Add ExampleLexParse test
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

---
+++ example_test.go
@@ -0,0 +1,136 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lexparse_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/ianlewis/lexparse"
+	"github.com/ianlewis/runeio"
+)
+
+const (
+	textType lexparse.LexemeType = iota
+	actionType
+)
+
+const (
+	actionLeft  = "{{"
+	actionRight = "}}"
+)
+
+// stateText parses normal text.
+func stateText(ctx context.Context, l *lexparse.Lexer) (lexparse.State, error) {
+	token, err := l.Find([]string{actionLeft})
+	lexeme := l.Lexeme(textType)
+	if lexeme.Value != "" {
+		l.Emit(lexeme)
+	}
+
+	var nextState lexparse.State
+	if token == actionLeft {
+		nextState = lexparse.StateFn(stateAction)
+	}
+	return nextState, err
+}
+
+// stateText lexes replacement actions (e.g. {{ var }}).
+func stateAction(ctx context.Context, l *lexparse.Lexer) (lexparse.State, error) {
+	// Discard the left brackets
+	if _, err := l.Discard(len(actionLeft)); err != nil {
+		return nil, err
+	}
+
+	// Find the right brackets.
+	token, err := l.Find([]string{actionRight})
+	lexeme := l.Lexeme(actionType)
+	if lexeme.Value != "" {
+		l.Emit(lexeme)
+	}
+
+	var nextState lexparse.State
+	if token == actionRight {
+		// Discard the right brackets
+		if _, err := l.Discard(len(actionRight)); err != nil {
+			return nil, err
+		}
+		nextState = lexparse.StateFn(stateText)
+	}
+	return nextState, err
+}
+
+// parseInit delegates to another parse function based on lexeme type.
+func parseInit(data map[string]string) lexparse.ParseFn[string] {
+	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
+		l := p.Peek()
+		if l == nil {
+			return nil, nil
+		}
+
+		switch l.Type {
+		case textType:
+			return parseText(data), nil
+		case actionType:
+			return parseAction(data), nil
+		default:
+			return nil, fmt.Errorf("unexpected type: %v", l.Type)
+		}
+	}
+}
+
+// parseText handles normal text.
+func parseText(data map[string]string) lexparse.ParseFn[string] {
+	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
+		l := p.Next()
+		if l == nil {
+			return nil, nil
+		}
+		p.Node(l.Value)
+		return parseInit(data), nil
+	}
+}
+
+// parseAction handles replacement actions (e.g. {{ var }})
+func parseAction(data map[string]string) lexparse.ParseFn[string] {
+	return func(ctx context.Context, p *lexparse.Parser[string]) (lexparse.ParseFn[string], error) {
+		l := p.Next()
+		if l == nil {
+			return nil, nil
+		}
+		v, ok := data[strings.TrimSpace(l.Value)]
+		if !ok {
+			return nil, fmt.Errorf("missing symbol: %q", l.Value)
+		}
+		p.Node(v)
+		return parseInit(data), nil
+	}
+}
+
+// ExampleLexParse implements a simple templating language.
+func ExampleLexParse() {
+	r := runeio.NewReader(strings.NewReader("Hello {{ subject }}!"))
+	data := map[string]string{"subject": "World"}
+	t, err := lexparse.LexParse(context.Background(), r, lexparse.StateFn(stateText), parseInit(data))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, n := range t.Root.Children {
+		fmt.Print(n.Value)
+	}
+	// Output: Hello World!
+}
